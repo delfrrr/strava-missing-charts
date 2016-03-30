72c7bb13fb83080c1950ab503eb344f0d306ffc4
Vova Bilonenko <bilonenko.v@gmail.com>
2016-03-30 23:32:50 +0200

    organize charts a bit

--- app/component/charts/index.js
+++ app/component/charts/index.js
@@ -18,6 +18,8 @@ const TF = 42 * DAY_LENGTH;
 // const TA = 7 * DAY_LENGTH;
 const CHART_SIZE = [1040, 300];
 const SVG_SIZE = [1040 + 20, 600];
+var line = d3shape.line();
+var timeScale = d3scale.scaleTime().range([0, CHART_SIZE[0]]);
 require('./charts.less');
 
 /**
@@ -32,67 +34,84 @@ var component = React.createClass({
             fitnessLine: null
         }
     },
+
+    _activities: null,
+
+    /**
+     * @param {d3.scaleTime} timeScale
+     */
+    _updateAxis: function (timeScale) {
+        var axisX = d3axis.axisBottom(timeScale);
+        var axisXYears = d3axis.axisBottom(timeScale);
+        axisX.tickArguments([d3time.timeMonth, 1]);
+        axisXYears.tickArguments([d3time.timeYear, 1]);
+        axisXYears.tickSize(0);
+        axisXYears.tickPadding(35);
+        axisX.tickPadding(10);
+        axisX.tickSizeInner(5);
+        axisX.tickSizeOuter(1);
+        axisX.tickFormat((value) => {
+            return moment(value).format('MMM');
+        });
+        axisXYears.tickFormat((value) => {
+            return moment(value).format('YYYY');
+        });
+        axisX(d3selection.select(this.refs.axisX));
+        axisXYears(d3selection.select(this.refs.axisXYears));
+    },
+
+    _updateCharts: function () {
+        /**
+         * @type {TrainingImpulse[]}
+         */
+        var trainingImpulses = Object.keys(this._activities).map((id) => {
+            return this._activities[id];
+        }).filter((activity) => {
+            return activity.suffer_score;
+        }).map((activity) => {
+            return [Date.parse(activity.start_date), activity.suffer_score];
+        });
+        var time = trainingImpulses[0][0];
+        var impulse;
+        var endTime = Date.now();
+        timeScale.domain([time, endTime]);
+
+        /**
+         * time, impulse sum pairs
+         * @type {Array.<{{0: number, 1: number}}>}
+         */
+        var fitnessAr = [];
+
+        while (time < endTime) {
+            impulse = 0;
+            trainingImpulses.forEach((impulseAr) => {
+                var impulseTime, impulseValue;
+                [impulseTime, impulseValue] = impulseAr;
+                if (impulseTime <= time) {
+                    impulse += impulseValue * Math.exp((impulseTime - time) / TF)
+                }
+            });
+            fitnessAr.push([timeScale(time), impulse]);
+            time += DAY_LENGTH;
+        }
+        var maxFitness = Math.max.apply(Math, fitnessAr.map((fitness) => {
+            return fitness[1];
+        }));
+        var fitnessScale = d3scale.scaleLinear()
+            .domain([0, maxFitness])
+            .range([0, CHART_SIZE[1]])
+        fitnessAr.forEach((fitness) => {
+            fitness[1] = fitnessScale(fitness[1]);
+        });
+        this._updateAxis(timeScale);
+        this.setState({
+            fitnessLine: line(fitnessAr)
+        });
+    },
     componentDidMount: function () {
         model.loadActivities().then((activities) => {
-            /**
-             * @type {TrainingImpulse[]}
-             */
-            var trainingImpulses = Object.keys(activities).map((id) => {
-                return activities[id];
-            }).filter((activity) => {
-                return activity.suffer_score;
-            }).map((activity) => {
-                return [Date.parse(activity.start_date), activity.suffer_score];
-            });
-            var time = trainingImpulses[0][0];
-            var impulse;
-            var endTime = Date.now();
-            var fitnessAr = [];
-            var dateScale = d3scale.scaleTime()
-                .domain([time, endTime])
-                .range([0, CHART_SIZE[0]])
-            while (time < endTime) {
-                impulse = 0;
-                trainingImpulses.forEach((impulseAr) => {
-                    var impulseTime, impulseValue;
-                    [impulseTime, impulseValue] = impulseAr;
-                    if (impulseTime <= time) {
-                        impulse += impulseValue * Math.exp((impulseTime - time) / TF)
-                    }
-                });
-                fitnessAr.push([dateScale(time), impulse]);
-                time += DAY_LENGTH;
-            }
-            var maxFitness = Math.max.apply(Math, fitnessAr.map((fitness) => {
-                return fitness[1];
-            }));
-            var fitnessScale = d3scale.scaleLinear()
-                .domain([0, maxFitness])
-                .range([0, CHART_SIZE[1]])
-            fitnessAr.forEach((fitness) => {
-                fitness[1] = fitnessScale(fitness[1]);
-            });
-            var line = d3shape.line();
-            var axisX = d3axis.axisBottom(dateScale);
-            var axisXYears = d3axis.axisBottom(dateScale);
-            axisX.tickArguments([d3time.timeMonth, 1]);
-            axisXYears.tickArguments([d3time.timeYear, 1]);
-            axisXYears.tickSize(0);
-            axisXYears.tickPadding(35);
-            axisX.tickPadding(10);
-            axisX.tickSizeInner(5);
-            axisX.tickSizeOuter(1);
-            axisX.tickFormat((value) => {
-                return moment(value).format('MMM');
-            });
-            axisXYears.tickFormat((value) => {
-                return moment(value).format('YYYY');
-            });
-            axisX(d3selection.select(this.refs.axisX));
-            axisXYears(d3selection.select(this.refs.axisXYears));
-            this.setState({
-                fitnessLine: line(fitnessAr)
-            });
+            this._activities = activities;
+            this._updateCharts();
         });
     },
     render: function () {
